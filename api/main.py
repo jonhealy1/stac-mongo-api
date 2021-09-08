54312d74aa78ddd31f54b03dca40e848591f4840
jonhealy1 <jonathan.d.healy@gmail.com>
2021-09-08 00:35:13 -0700

    add dummy collection route

--- api/main.py
+++ api/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/collections/{collection_id}")
+async def get_collection(collection_id):
+    return {"collection_id": collection_id}
