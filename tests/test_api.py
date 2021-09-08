3f008fa9a43c6de5f16b821a3a5d0ace7622e422
jonhealy1 <jonathan.d.healy@gmail.com>
2021-09-08 12:45:17 -0700

    create get all collections test

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,3 +10,15 @@ def test_get_collection(app_client):
     assert resp.status_code == 200
     resp_json = resp.json()
     assert resp_json["content"]["id"] == collection_id
+
+
+def test_get_all_collections(app_client):
+    f = open('tests/test_data/collection.json')
+    collection = json.load(f)
+    collection_id = collection["id"]
+    resp = app_client.post(f"/collections/")
+
+    resp = app_client.get(f"/collections")
+    assert resp.status_code == 200
+    resp_json = resp.json()
+    assert len(resp_json) >= 1
