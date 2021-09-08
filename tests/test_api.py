357adf98301d84af4ea3114a68c6bf06fb59f90f
jonhealy1 <jonathan.d.healy@gmail.com>
2021-09-08 12:39:35 -0700

    load collection in tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,12 @@
+import json
+
 def test_get_collection(app_client):
-    collection = "sentinel-2"
-    resp = app_client.get(f"/collections/{collection}")
+    f = open('tests/test_data/collection.json')
+    collection = json.load(f)
+    collection_id = collection["id"]
+    resp = app_client.post(f"/collections/")
+
+    resp = app_client.get(f"/collections/{collection_id}")
     assert resp.status_code == 200
     resp_json = resp.json()
-    assert resp_json["content"]["id"] == collection
+    assert resp_json["content"]["id"] == collection_id
