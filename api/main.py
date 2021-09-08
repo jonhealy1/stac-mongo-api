3e52441d761a0f78328fc5d83354de5f3b1bb9ba
jonhealy1 <jonathan.d.healy@gmail.com>
2021-09-08 00:40:08 -0700

    create all collections route

--- api/main.py
+++ api/main.py
@@ -3,8 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root_catalog():
+    return {"root_catalog": "Hello World"}
+
+@app.get("/collections")
+async def get_all_collections():
+    return {"all_collections": "All Collections"}
 
 @app.get("/collections/{collection_id}")
 async def get_collection(collection_id):
