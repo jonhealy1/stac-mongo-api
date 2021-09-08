a140ba2ca7035ddffb3831b924585f3eebe44995
jonhealy1 <jonathan.d.healy@gmail.com>
2021-09-08 12:52:00 -0700

    load test collection only if it's not in the db already

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,24 +1,21 @@
 import json
 
 def test_get_collection(app_client):
-    f = open('tests/test_data/collection.json')
-    collection = json.load(f)
-    collection_id = collection["id"]
-    resp = app_client.post(f"/collections/")
-
+    collection_id = "sentinel-2"
     resp = app_client.get(f"/collections/{collection_id}")
-    assert resp.status_code == 200
+    if resp.status_code != 200:
+        f = open('tests/test_data/collection.json')
+        collection = json.load(f)
+        resp = app_client.post(f"/collections/", json=collection)
     resp_json = resp.json()
     assert resp_json["content"]["id"] == collection_id
 
 
 def test_get_all_collections(app_client):
-    f = open('tests/test_data/collection.json')
-    collection = json.load(f)
-    collection_id = collection["id"]
-    resp = app_client.post(f"/collections/")
-
     resp = app_client.get(f"/collections")
-    assert resp.status_code == 200
+    if resp.status_code != 200:
+        f = open('tests/test_data/collection.json')
+        collection = json.load(f)
+        resp = app_client.post(f"/collections/", json=collection)
     resp_json = resp.json()
     assert len(resp_json) >= 1
